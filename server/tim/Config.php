0417bf71d7e977cdfb9849777adcfb4ea34ff160
xbw12138 <xbw@ecfun.cc>
2018-03-03 16:08:34 +0800

    init
    使用绝对路径

--- server/tim/Config.php
+++ server/tim/Config.php
@@ -3,6 +3,6 @@ header('Content-type: application/json; charset=UTF-8');
 // 设置REST API调用基本参数
 $sdkappid = 0;
 $identifier = "admin";
-$private_key_path = "/opt/lampp/htdocs/petpet/tim/private.pem";
-$signature = "/opt/lampp/htdocs/petpet/tim/signature/linux-signature64";
+$private_key_path = dirname(__FILE__)."/private.pem";
+$signature = dirname(__FILE__)."/signature/linux-signature64";
 $usersig = '';//设置管理员usersig	180天失效
